a5f4b277f2ecb5599f276f482fb47d3fe102b225
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-06-11 15:30:13 +0200

    added the remove, removeKey, removeValue methods to the Arr class

--- src/tad/utils/Arr.php
+++ src/tad/utils/Arr.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace tad\utils;
+
 
 use \tad\utils\Str;
 
@@ -28,5 +30,58 @@ class Arr
             $buffer[$camelBackKey] = $value;
         }
         return $buffer;
+    }
+    
+    /**
+     * Remove an element from a numerical array
+     *
+     * @param  array $arr   The array to remove the value from
+     * @param  mixed $value The value to remove from the array
+     *
+     * @return array        The original array minus the removed value
+     */
+    public static function remove(array $arr, $value)
+    {
+        return array_values(array_diff($arr, array(
+            $value
+        )));
+    }
+    
+    /**
+     * Remove key/value pairs on a key base from an array.
+     *
+     * Usage is Arr::removeKey($arr, 'one', 'two')
+     *
+     * @return array The array minus the removed elements
+     */
+    public static function removeKey()
+    {
+        $args = func_get_args();
+        $arr = $args[0];
+        $keys = array_slice($args, 1);
+        
+        foreach ($arr as $k => $v) {
+            if (in_array($k, $keys)) unset($arr[$k]);
+        }
+        return $arr;
+    }
+    
+    /**
+     * Remove key/value pairs from an array on a value base from an array.
+     *
+     * Usage is Arr::removeValue($arr, 'one', 'two')
+     *
+     * @return array The array minus the removed elements
+     */
+    public static function removeValue()
+    {
+        $args = func_get_args();
+        $arr = $args[0];
+        $values = array_slice($args, 1);
+        
+        foreach ($arr as $k => $v) {
+            if (in_array($v, $values)) unset($arr[$k]);
+        }
+        return $arr;
     }
 }

c04030b9d158a0242b4b0ac217803d285158bee1
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2014-08-21 16:01:41 +0200

    added the tad_Object to the utility classes

--- src/tad_Object.php
+++ src/tad_Object.php
@@ -1,5 +1,11 @@
 <?php
 
-class tad_Object {
+class tad_Object
+{
+    protected $f;
 
-} 
+    public function setFunctionsAdapter(tad_FunctionsAdapterInterface $functionsAdapter = null)
+    {
+        $this->f = $functionsAdapter ? $functionsAdapter : new tad_FunctionsAdapter();
+    }
+}
